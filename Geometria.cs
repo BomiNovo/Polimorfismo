40bfb0cf209138267363790d22bcb7b2abdb9e77
43077509857 <43077509857@ECE337N13SN790>
2026-09-11 11:34:38 -0300

    Adicionar arquivos de projeto.

---
+++ Geometria.cs
@@ -0,0 +1,20 @@
+﻿namespace Polimorfismo
+{
+    class Geometria
+    {
+        public double CalcularArea( double lado)
+        {
+            return lado * lado;
+        }
+
+        public double CalcularArea(double basee, double altura)
+        {
+            return basee * altura;
+        }
+
+        public double CalcularArea(double raio, bool isCirculo)
+        {
+            return Math.PI * raio * raio;
+        }
+    }
+}
